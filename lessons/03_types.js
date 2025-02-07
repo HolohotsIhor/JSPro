b6cf74440ab72edfe9eac2f871433b2516c9003e
airSlateHolohots <ihor.holohots@airslate.com>
2025-02-07 11:47:20 +0200

    Fix for homework 3

--- lessons/03_types.js
+++ lessons/03_types.js
@@ -1,23 +1,29 @@
 console.log("3.1");
 console.log("\nПривмітивні типи:");
-console.log("undefined:", undefined);
-console.log("null:", null); // "object" (из-за бага в JS)
-console.log("boolean:", true);
-console.log("number:", 42);
-console.log("bigint:", 9007199254740991n);
-console.log("string:", "Hello, JS!");
-console.log("symbol:", Symbol("id"));
+console.log("undefined:", typeof undefined);
+console.log("null:", typeof null); // "object" (из-за бага в JS)
+console.log("boolean:", typeof true);
+console.log("number:", typeof 42);
+console.log("bigint:", typeof 9007199254740991n);
+console.log("string:", typeof "Hello, JS!");
+console.log("symbol:", typeof Symbol("id"));
 
 console.log("\nСкладні типи:");
-console.log("object: ", { key: "value" });
-console.log("array (також object): ", [1, 2, 3]);
-console.log("function (підтип object): ", function() {});
+console.log("object: ", typeof { key: "value" });
+console.log("array (також object): ", typeof [1, 2, 3]);
+console.log("array (Array.isArray). Перевірка, що об'єкт є массивом: ", Array.isArray([1, 2, 3]));
+console.log("function (підтип object): ", typeof function() {});
 
 console.log("\n\n3.2");
 const str1 = prompt("Введіть перший рядок:");
 const str2 = prompt("Введіть другий рядок:");
 const str3 = prompt("Введіть третій рядок:");
-console.log(`${str3}\n${str1}\n${str2}`);
+const array = [str1, str2, str3]
+    .map(value => ({ value, sort: Math.random() })) // Create array of object
+    .sort((a, b) => a.sort - b.sort) // Sort by random value
+    .map(obj => obj.value); // Remove random value
+
+console.log(`${array[0]}\n${array[1]}\n${array[2]}`);
 
 console.log("\n\n3.3");
 const number = prompt("Введіть п'ятизначне число:");
