ba55839d5dbb04a90a2411b9459cef2aa6961ab9
airSlateHolohots <ihor.holohots@airslate.com>
2025-03-19 17:08:33 +0200

    Form valid

--- js/13_homework-form-regular.js
+++ js/13_homework-form-regular.js
@@ -1,12 +1,39 @@
 'use strict'
 
 const regExp = {
-    name: /^[a-zA-Z]+$/gi,
+    name: /^[a-z]{2,}$/i,
+    email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    phone: /^\+?380\d{9}$/,
+}
+let isValid = true;
+
+const removeErrors = (items) => {
+    items.length > 0 &&
+        items.forEach(item => {
+            item.remove();
+        })
+}
+
+const setError = (parentId) => {
+    const errorEl = document.createElement('div');
+    const parent = document.querySelector(`#${parentId}`);
+
+    errorEl.classList.add('error-message');
+    errorEl.innerText = "Please input correct data";
+
+    isValid = false;
+    parent.appendChild(errorEl);
+}
+
+const showValidData = (form, data) => {
+    form.remove();
+    document.body.innerHTML = JSON.stringify(data, null, 2);
 }
 
 const initForm = () => {
     const trigger = document.querySelector('.js-form-button');
     const formElements = document.forms.formValidation;
+    const form = document.querySelector('#formValidation');
 
     trigger.addEventListener('click', () => {
         const firstname = formElements.firstname.value;
@@ -17,12 +44,23 @@ const initForm = () => {
         const gender = formElements.gender.value;
         const languages = [];
         const message = formElements.message.value;
+        let formData = {}
+
+        isValid = true;
+        removeErrors(document.querySelectorAll('.error-message'));
 
         for (let i = 0; i < formElements.languages.length; i++) if (formElements.languages[i].checked) languages.push(formElements.languages[i].value);
 
-        if (firstname.match(regExp.name)) console.log(true);
-        if (lastname.match(regExp.name)) console.log(true);
-        else console.log(false);
+        firstname.match(regExp.name) ? formData = { ...formData, firstname } : setError('firstname-parent');
+        lastname.match(regExp.name) ? formData = { ...formData, lastname } : setError('lastname-parent');
+        email.match(regExp.email) ? formData = { ...formData, email } : setError('email-parent');
+        phone.match(regExp.phone) ? formData = { ...formData, phone } : setError('phone-parent');
+        city !== '-1' ? formData = { ...formData, city } : setError('city-parent');
+        gender ? formData = { ...formData, gender } : setError('gender-parent');
+        languages.length > 0 ? formData = { ...formData, languages } : setError('languages-parent');
+        message ? formData = { ...formData, message } : setError('message-parent');
+
+        isValid ? showValidData(form, formData) : console.log('false valid');
     })
 }
 
